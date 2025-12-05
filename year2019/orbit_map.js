8d532c619704a76af7c5d6913818bef24f6d93cc
Kiranmai Lalam <kiranmailalam@Kiranmais-MacBook-Pro.local>
2025-12-05 12:51:42 +0530

    part1 of day 6

--- year2019/orbit_map.js
+++ year2019/orbit_map.js
@@ -1,34 +1,29 @@
 const orbits = {};
 
 const mapOrbits = (data) => {
+  console.log(data.length);
   for (let i = 0; i < data.length; i++) {
     const [inside, outside] = data[i].split(")");
-    (!orbits[inside]) ? orbits[inside] = [outside] : orbits[inside].push(outside);
-    if (i === data.length - 1) {
-      orbits[outside] = [];
-    }
+    (!orbits[inside]) ? (orbits[inside] = [outside]) : orbits[inside].push(outside);
   }
 }
 
 const orbitCount = (orbit) => {
   const keys = Object.keys(orbits);
   const values = Object.values(orbits);
-  // console.log(orbit);
   let rOrbit = orbit;
   let count = -1;
   while(rOrbit) {
     rOrbit = keys[values.findIndex(lorbit => lorbit.includes(rOrbit))];
     count++;
   }
-  // console.log("count", count);
   return count;
 }
 
 const part1 = () => {
-  // const data = Deno.readTextFileSync("./input.txt").split("\n");
-  const data = ["COM)B","B)C","C)D","D)E","E)F","B)G","G)H","G)M","M)N","D)I","E)J","J)K","K)L"];
+  const data = Deno.readTextFileSync("./input.txt").split("\n");
+  // const data = ["B)C","C)D","D)E","COM)B","E)F","B)G","G)H","D)I","E)J","J)K","K)L"];
   mapOrbits(data);
-  console.log(orbits);
   const keys = Object.values(orbits).flat();
   return keys.reduce((total, orbit) => total + orbitCount(orbit), 0);
 }
